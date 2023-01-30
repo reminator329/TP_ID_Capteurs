7a9f20de4bb03e7fd0b2d408fb0b3cf875b146ca
Didier Laborie <dlaborie@inra.local>
2023-01-30 21:45:23 +0100

    clean

--- DistanceSender/src/main/java/Main.java
+++ DistanceSender/src/main/java/Main.java
@@ -3,10 +3,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Enumeration;
 import java.util.UUID;
 
 public class Main {
